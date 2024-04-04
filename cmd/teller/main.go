6cc22a79bc147ed50864febf9f25c7025335c745
hashhavoccat <162091348+hashhavoccat@users.noreply.github.com>
2024-04-03 22:33:20 -0700

    fix default log level to error

--- cmd/teller/main.go
+++ cmd/teller/main.go
@@ -8,8 +8,25 @@ import (
 )
 
 func main() {
+	logLevelEnv := os.Getenv("TELLER_LOG_LEVEL")
+	logLevel := log.ErrorLevel
+
+	switch logLevelEnv {
+	case "DEBUG":
+		logLevel = log.DebugLevel
+	case "INFO":
+		logLevel = log.InfoLevel
+	case "WARN":
+		logLevel = log.WarnLevel
+	case "ERROR":
+		logLevel = log.ErrorLevel
+	case "FATAL":
+		logLevel = log.FatalLevel
+	}
+
 	glog := log.Logger{
 		TimeFormat: "15:04:05",
+		Level:      logLevel,
 		Caller:     0,
 		Writer: &log.ConsoleWriter{
 			ColorOutput:    true,
